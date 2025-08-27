6459917cdaa387b9a7d05126b367ec73339d8fa1
Gustavo Kamitani <gustavodocarmokamitani@gmail.com>
2025-08-27 17:55:03 -0300

    fix: api

--- src/client/services/api.js
+++ src/client/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://localhost:3000/api/v1',
-  baseURL: 'https://gecebs-backend.up.railway.app/api/v1',
+  baseURL: 'https://gecebs-production.up.railway.app/api/v1',
 });
 
 // Interceptor para adicionar o token de autenticação
